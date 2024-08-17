87a670e501141a865a264a0d2e4332c4c842debf
Abdi Deresa <Abdishegute@gmail.com>
2024-08-16 20:55:16 -0700

    addded function and css

--- index.js
+++ index.js
@@ -17,8 +17,25 @@ function initializeSlider() {
   }
 }
 
-function showSlide() {}
+function showSlide(index) {
+  if (index >= slides.length) {
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = slides.length - 1;
+  }
+  slides.forEach((slide) => {
+    slide.classList.remove("displaySlide");
+  });
+  slides[slideIndex].classList.add("displaySlide");
+}
 
-function prevSlide() {}
+function prevSlide() {
+  clearInterval(intervalId);
+  slideIndex--;
+  showSlide(slideIndex);
+}
 
-function nextSlide() {}
+function nextSlide() {
+  slideIndex++;
+  showSlide(slideIndex);
+}
